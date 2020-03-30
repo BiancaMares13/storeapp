0342c855944ae3ef581b98ad57f30eeea633ec69
GeorgeT <trifangeorge@yahoo.com>
2020-03-30 18:45:15 +0300

    storeapp#9: updated

--- src/main/java/ro/web/store/service/ProductService.java
+++ src/main/java/ro/web/store/service/ProductService.java
@@ -13,18 +13,18 @@ public class ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 
-	Product product = new Product(4, "table2", "small coffe table", 1000,
+	Product mockProduct = new Product(4, "table2", "small coffe table", 1000,
 		"https://imgur.com/gallery/eHKcitf", "Tables");
 
 	public Product addProduct(Product product) {
-		return productRepository.save(this.product);
+		return productRepository.save(this.mockProduct);
 	}
 
 	public Product updateProduct(Product product) {
-		return productRepository.save(this.product);
+		return productRepository.save(this.mockProduct);
 	}
 
 	public void deleteProduct(Product product) {
-		productRepository.deleteById(this.product.getIdProduct());
+		productRepository.deleteById(this.mockProduct.getIdProduct());
 	}
 }
